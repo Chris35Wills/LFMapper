7d1bc4cca19392c6453b9c3caff0310d8276a646
CN Williams <cw14910@bristol.ac.uk>
2016-05-26 11:50:51 +0100

    np version check

--- crevassemap/__init__.py
+++ crevassemap/__init__.py
@@ -2,5 +2,5 @@ from distutils.version import StrictVersion
 import numpy as np
 import sys
 
-if (np.version.version>=StrictVersion('1.8.0')):
+if not (np.version.version>=StrictVersion('1.8.0')):
 	sys.exit("Update numpy to at least version 1.8.0")
