ada2b9b41d5628c7a811575d17f82145e398b242
CN Williams <cw14910@bristol.ac.uk>
2016-05-27 12:25:38 +0100

    set matplotlib backend to work is DISPLAY is not set

--- crevassemap/__init__.py
+++ crevassemap/__init__.py
@@ -1,6 +1,10 @@
 from distutils.version import StrictVersion
 import numpy as np
+import matplotlib
 import sys
 
 if not (np.version.version>=StrictVersion('1.8.0')):
 	sys.exit("Update numpy to at least version 1.8.0")
+
+# to ensure plotting works if diaply isn't set: http://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined
+matplotlib.use('Agg')
